a7263fe6c434e473cac97ad13c1ec0fa94678628
RodrigoDev <rpuertadelpozo@gmail.com>
2024-10-21 10:39:38 +0200

    getHumanChoice and getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+humanScore = 0
+computerScore = 0
+
+function getComputeChoice(){
+
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if(randomNumber === 0){
+        return "rock";
+    } else if(randomNumber === 1){
+        return "paper";
+    } else if(randomNumber === 2){
+        return "scissors";
+    }   
+
+}
+function getHumanChoice(){
+    let userChoice = prompt("Elige piedra,papel o tijeras").toLowerCase();
+    if (userChoice === "piedra" || userChoice === "papel" || userChoice === "tijeras"){
+        return userChoice;
+      }else{
+        console.log("Opcion invalida");
+        return getHumanChoice();
+      }
+}
+console.log(getHumanChoice());
